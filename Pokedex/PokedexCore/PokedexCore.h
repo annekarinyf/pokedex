5a10d138406303ac79385d1920096c28508bd793
Anne Kariny Silva Freitas <annekarinyfreitas@gmail.com>
2023-02-11 07:57:07 -0300

    Create project

---
+++ Pokedex/PokedexCore/PokedexCore.h
@@ -0,0 +1,18 @@
+//
+//  PokedexCore.h
+//  PokedexCore
+//
+//  Created by Anne Kariny Silva Freitas on 11/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokedexCore.
+FOUNDATION_EXPORT double PokedexCoreVersionNumber;
+
+//! Project version string for PokedexCore.
+FOUNDATION_EXPORT const unsigned char PokedexCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokedexCore/PublicHeader.h>
+
+
